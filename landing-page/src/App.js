a0be4bd744a157618623fbb41a7872aaab202e96
nicprkr <nicolarparker@gmail.com>
2018-01-23 00:56:54 -0800

    changed landing page files

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,27 +1,108 @@
 import React, { Component } from 'react';
-import sky from "./assets/sky.svg";
-import clouds from "./assets/clouds.svg";
-import sun from "./assets/sun.svg";
-import mountains from "./assets/mountains.svg";
-import foothills from "./assets/foothills.svg";
-import river from "./assets/river.svg";
-import trees from "./assets/trees.svg";
-import foothills1 from "./assets/foothills_1.svg";
+import './App.css';
+import evelyn from "./assets/evelyn.png";
+import nicola from "./assets/nicola.png";
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            nicCard: false,
+            evCard: false
+        }
+    }
+    
   render() {
+      var nicCard = null;
+      var evCard = null;
+      if (this.state.nicCard === false){
+          nicCard = (
+            <div className="card">
+              <div className="card-header">bcit d3</div>
+              <hr className="hr-pretty"/>
+            <div class="card-name">nicola parker</div>
+            <img src={nicola} style={photo} />
+            <div className="card-desc">Full Stack Development
+              <div class="italics"> & frisbees</div></div>
+            <button
+              className="btn"
+              onClick={()=>this.setState({ nicCard: true })}>ABOUT ME</button>
+        </div>
+          )
+      } else if (this.state.nicCard === true){
+          nicCard = (
+          <div className="card">
+              <div className="card-header">nicola parker</div>
+              <hr className="hr-pretty"/>
+                <div className="icons">
+                    <div class="placeholder"></div>
+                    <div class="placeholder"></div>
+                </div>
+<button className="btn">PORTFOLIO</button>
+<div class="info">
+<div class="info-header">TOP SKILLS</div>
+<div>javascript, wordpress, logo design</div>
+<div class="info-header">DREAM JOB</div>
+<div>developer at amazon</div>
+<div class="info-header">IDEAL WEEKEND</div>
+<div>hiking, frisbee, reading</div>
+<div class="info-header">2018 GOAL</div>
+<div>learn node.js</div>
+</div>
+          <div
+              className="placeholder"
+              onClick={()=>this.setState({ nicCard: false })}>FLIP</div>
+            </div>
+          )
+      }
+      if (this.state.evCard === false){
+          evCard = (
+            <div className="card">
+              <div className="card-header">bcit d3</div>
+              <hr className="hr-pretty"/>
+            <div class="card-name">evelyn cranston</div>
+            <img src={evelyn} style={photo} />
+            <div className="card-desc">Front End Design 
+              <div class="italics"> & bikes</div></div>
+            <button
+              className="btn"
+              onClick={()=>this.setState({ evCard: true })}>ABOUT ME</button>
+        </div>
+          )
+      } else if (this.state.evCard === true){
+          evCard = (
+          <div className="card">
+            <div className="card-header">evelyn cranston</div>
+              <hr className="hr-pretty"/>
+                <div className="icons">
+                    <div class="placeholder"></div>
+                    <div class="placeholder"></div>
+                </div>
+<button className="btn">PORTFOLIO</button>
+<div class="info">
+<div class="info-header">TOP SKILLS</div>
+<div>javascript, design, react</div>
+<div class="info-header">DREAM JOB</div>
+<div>overlord of slack</div>
+<div class="info-header">IDEAL WEEKEND</div>
+<div>getting coffee, adventures, reading</div>
+<div class="info-header">2018 GOAL</div>
+<div>learn node.js</div>
+</div>
+          <div
+              className="placeholder"
+              onClick={()=>this.setState({ evCard: false })}>FLIP</div>
+            </div>
+          )
+      }
     return (
-      <div>
-        <div className="column">
-            <img src={sky} style={skyStyle}/>
-            <img src={sun} style={sunStyle} />
-            <img src={clouds} style={cloudStyle} />
-            <img src={mountains} style={mtnStyle} />
-        <div style={foregroundStyle}>
-            <img src={trees} style={treesStyle} />
-            <img src={foothills} style={fthStyle} />
-            <img src={river} style={riverStyle} />
-            <img src={foothills1} style={fth1Style} />
+      <div style={container}>
+        <div className="pageContainer">
+        <div className="pageHeader">Nicola & Evelyn</div>
+        <div className="content">
+        {nicCard}
+        {evCard}
+        
         </div>
         </div>
         </div>
@@ -31,51 +112,12 @@ class App extends Component {
 
 export default App;
 
-const skyStyle = {
-    width: '100%',
-    zIndex: '-100',
-    position: 'absolute',
-    top: 0,
-    left: 0
-};
-const cloudStyle = {
-    width: '120%',
-    position: 'absolute',
-    top: '20vh',
-    right: '-2vw'
-};
-const sunStyle = {
-    width: '10%',
-    position: 'absolute',
-    right: '20vw',
-    top: '10vh'
-};
-const mtnStyle = {
-    width: '100%',
-    position: 'absolute',
-    top: '30vh'
-};
-const fthStyle = {
-    width: '100%',
-    position: 'absolute',
+const container = {
+    width: "100%",
+    height: "100%",
+    color: "#1D41A8"
 }
-const riverStyle = {
-    width: '100%',
-    position: 'absolute',
-    top: '6vh'
-}
-const treesStyle = {
-    width: '100%',
-    position: 'absolute',
-    bottom: '-10vh'
-}
-const fth1Style = {
-    width:'100%',
-    position: 'absolute',
-    top: '4vh'
-}
-const foregroundStyle = {
-    width: '100%',
-    position: 'absolute',
-    top: '36vh',
+const photo = {
+    width: "135px",
+    height: "182px"
 }
