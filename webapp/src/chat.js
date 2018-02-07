c8656cb3899f16450878a8d21077620173d0358d
evelyn <evelyncranston@Live.ca>
2018-02-07 11:51:04 -0800

    Fixing typo in chat.js

--- webapp/src/chat.js
+++ webapp/src/chat.js
@@ -1,4 +1,4 @@
--import React, { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import mySocket from 'socket.io-client';
 import sendIcon from './assets/send.png';
